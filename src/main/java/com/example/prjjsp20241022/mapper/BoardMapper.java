37fc6d43ad9258e3352c246e342846caccdd140a
Jin19121 <jinkim19121@gmail.com>
2024-10-22 15:19:52 +0900

    list 한 페이지에 10개 표시

--- src/main/java/com/example/prjjsp20241022/mapper/BoardMapper.java
+++ src/main/java/com/example/prjjsp20241022/mapper/BoardMapper.java
@@ -42,4 +42,11 @@ public interface BoardMapper {
             WHERE id=#{id}
             """)
     int update(Board board);
+
+    @Select("""
+            SELECT * FROM board
+            ORDER BY id DESC
+            LIMIT #{offset}, 10
+            """)
+    List<Board> selectAllPaging(Integer offset);
 }
