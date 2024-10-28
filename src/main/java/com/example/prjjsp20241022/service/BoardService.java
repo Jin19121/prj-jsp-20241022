1082db87448da023c932d630df06663f31a91f69
Jin19121 <jinkim19121@gmail.com>
2024-10-28 10:31:58 +0900

    검색 결과 + pagination : 마지막 페이지 계산

--- src/main/java/com/example/prjjsp20241022/service/BoardService.java
+++ src/main/java/com/example/prjjsp20241022/service/BoardService.java
@@ -31,7 +31,7 @@ public class BoardService {
         Map<String, Object> map = new HashMap<>();
 
         //페이지 관련 정보들
-        Integer countAll = mapper.countAll();
+        Integer countAll = mapper.countAll(searchTarget, keyword);
         Integer lastPageNumber = (countAll - 1) / 10 + 1; //마지막 페이지
         Integer rightPageNumber = ((page - 1) / 10 + 1) * 10; //현재 페이지 기준 오른쪽 끝 페이지 번호
         Integer leftPageNumber = rightPageNumber - 9; //현재 페이지 기준 왼쪽 끝
